2a2c0ede7401fabb13fab4da288b352dc422a6b1
Piyush singh <thisthat587@gmail.com>
2023-12-11 19:46:07 +0530

    .....

--- imageselection/imagecropping.js
+++ imageselection/imagecropping.js
@@ -2,41 +2,75 @@ const sharp = require('sharp');
 const fs = require('fs');
 const path = require('path');
 
-let XSTART = 244; // Starting X position
+// let XSTART = 244; // Starting X position
 const TOP = 130; // Fixed top position
 const WIDTH = 100; // Width of each column
 const HEIGHT = 370; // Height of the crop
-
-async function cropImageIntoColumns(imagePath, numColumns) {
-    try {
-        const image = sharp(imagePath);
-        const metadata = await image.metadata();
-
-        if (XSTART + WIDTH * numColumns > metadata.width || TOP + HEIGHT > metadata.height) {
-            throw new Error('Crop area exceeds image bounds');
+let left = 244;
+function cropImageIntoColumns(imagePath, numColumns, callback) {
+    sharp(imagePath).metadata((err, metadata) => {
+        if (err) {
+            return callback(err);
         }
 
-        let promises = [];
+        console.log(metadata);
+
+        // if (XSTART + WIDTH * numColumns > metadata.width || TOP + HEIGHT > metadata.height) {
+        //     return callback(new Error('Crop area exceeds image bounds'));
+        // }
+
+        let tasksCompleted = 0;
 
         for (let i = 0; i < numColumns; i++) {
-            let left = XSTART + i * WIDTH;
-            const outputPath = path.join(__dirname, `cropped_column_${i + 1}.jpg`);
+            if (i === 0) {
+                left = 244;
+            }
+            else if (i === 1) {
+                left = 355;
+            }
+            else if (i === 2) {
+                left = 475;
+            }
+            else if (i === 3) {
+                left = 580;
+            }
+            else if (i === 4) {
+                left = 755;
+            }
+            else if (i === 5) {
+                left = 860;
+            }
+            else if (i === 6) {
+                left = 930;
+            }
+            else {
+                left = 1030;
+            }
+            const outputPath = path.join(__dirname, `cropped_column_${i + 1}.jpeg`);
 
-            promises.push(
-                image
-                    .extract({ left, top: TOP, width: WIDTH, height: HEIGHT })
-                    .toFile(outputPath)
-            );
+            sharp(imagePath)
+                .extract({ left, top: TOP, width: WIDTH, height: HEIGHT })
+                .toFile(outputPath, (error) => {
+                    if (error) {
+                        return callback(error);
+                    }
+                    tasksCompleted++;
+                    if (tasksCompleted === numColumns) {
+                        console.log('Cropping completed!');
+                        callback(null);
+                    }
+                });
         }
-
-        await Promise.all(promises);
-        console.log('Cropping completed!');
-    } catch (error) {
-        console.error('Error during image cropping:', error.message);
-    }
+    });
 }
 
 const imagePath = 'WhatsApp Image 2023-12-10 at 7.45.40 PM.jpeg'; // Replace with your image path
-const numColumns = 2; // Adjust as needed
+const numColumns = 8; // Adjust as needed
 
-cropImageIntoColumns(imagePath, numColumns);
+cropImageIntoColumns(imagePath, numColumns, (err) => {
+    if (err) {
+        console.error('Error during image cropping:', err.message);
+    } else {
+        console.log('All images cropped successfully');
+    }
+});
