f393a37f18ecda731b22b26899ea312eb775fff9
thisthat587 <thisthat587@gmail.com>
2023-12-22 21:24:31 +0530

    .

--- render.js
+++ render.js
@@ -6,16 +6,16 @@ class Elements {
     stdname = document.getElementById('name');
     fname = document.getElementById('fname');
     mobile = document.getElementById('mobile');
-
 }
 
+const e = new Elements;
 class Search {
     #inputValue;
     #query;
-    connection;
+    #connection;
     constructor() {
-        document.getElementById('name').focus();
-        this.connection = mysql.createConnection({
+        e.stdname.focus();
+        this.#connection = mysql.createConnection({
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
@@ -23,9 +23,9 @@ class Search {
         });
     }
 
-    showStdlistDiv(){
-        document.getElementById('stdList-div').style.display='';
-        document.getElementById('enquiry-div').style.display='none';
+    showStdlistDiv() {
+        document.getElementById('stdList-div').style.display = '';
+        document.getElementById('enquiry-div').style.display = 'none';
     }
     // async connectingMysql() {
     //     try {
@@ -54,22 +54,20 @@ class Search {
             this.#query = `SELECT admno, name, fname, class, section, roll, fmob, session, active, transport FROM tbl_admission WHERE session = "2023-2024" AND active = 1 AND fmob=${this.#inputValue};`;
         }
         // this.connectingMysql();
-        // this.getQuery();
+        this.getQuery();
     }
 
 
     getQuery() {
         // console.log(this.#query);
-        this.connection.query(this.#query, (error, results) => {
+        this.#connection.query(this.#query, (error, results) => {
             if (error) {
                 console.log("Some error occured...", error)
                 return;
             }
             if (results.length > 0) {
-                console.log("Result : BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBKHL.HIO;HBUJVHYVFUU");
+                // console.log("Result : BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBKHL.HIO;HBUJVHYVFUU");
                 console.log("Result : ", result);
-                document.getElementById('stdList-div').innerHTML = `<h3>${results[0].name}</h3>`;
-
             }
         })
     }
@@ -102,10 +100,9 @@ class Search {
     }
 }
 // SearchObj.getQuery();
-const SearchObj = new Search;
-const e = new Elements;
 
 function catchsearchBtn() {
+    const SearchObj = new Search;
     SearchObj.showStdlistDiv();
     SearchObj.sendinputValuetoSearch();
     SearchObj.getQuery();
